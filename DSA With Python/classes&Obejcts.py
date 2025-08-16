a7d3cb2c447cead33e03b2eaccb5c718a22dfbd1
Ankit Bhagat <bhagatankit502@gmail.com>
2025-08-16 19:25:58 +0530

    First commit

--- DSA With Python/classes&Obejcts.py
+++
@@ -1,9 +0,0 @@
-# Classes and Objects :
-
-#  Class is way to implement Encapsulation in Python.
-#  Class is a blueprint for creating objects.
-#  Object is an instance of a class.
-
-# l1 = [10,20,30]
-# l1.append(40)
-# l1.show() # AttributeError: 'list' object has no attribute 'show'
