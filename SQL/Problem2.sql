2e9f70d01d87892b359d40a7c0d2fda7a908db5f
Ankit Bhagat <bhagatankit502@gmail.com>
2025-09-14 17:15:07 +0530

    SQl ongoing~

--- SQL/Problem2.sql
+++ SQL/Problem2.sql
@@ -9,3 +9,12 @@ marks INT NOT NULL,
 grade VARCHAR(1),
 city VARCHAR(20)
 );
+
+INSERT INTO student
+(rollno,name,marks,grade,city)VALUES 
+(101,'Anil',78,'C','Pune'),
+(102,'Bhumika',93,'A','Mumbai'),
+(103,'Chetan',85,'A','Mumbai'),
+(104,'Anil',96,'A','Delhi'),
+(105,'Dhruv',12,'F','Delhi'),
+(106,'Pratik',82,'B','Delhi');
