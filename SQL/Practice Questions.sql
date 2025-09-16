b8a0ca9fd57c9ccdaf4458fe45b79092633aff54
Ankit Bhagat <bhagatankit502@gmail.com>
2025-09-16 21:50:33 +0530

    SQL Ongoing~

--- SQL/Practice Questions.sql
+++ SQL/Practice Questions.sql
@@ -1,3 +1,7 @@
 -- Write a Query to find avg marks in each city in ascending order. 
 USE college_db;
 SELECT city, AVG(marks) FROM student GROUP BY city ORDER BY city;
+
+SELECT * FROM student;
+
+SELECT grade FROM student GROUP BY grade ORDER BY grade;
