12295a40c2688e4be2ba24f21969af27f1783f75
Ankit Bhagat <bhagatankit502@gmail.com>
2025-09-17 19:04:07 +0530

    SQL Ongoing~

--- SQL/Practice_Questions.sql
+++ SQL/Practice_Questions.sql
@@ -5,3 +5,21 @@ SELECT city, AVG(marks) FROM student GROUP BY city ORDER BY city;
 SELECT * FROM student;
 
 SELECT grade FROM student GROUP BY grade ORDER BY grade;
+
+SELECT grade , COUNT(rollno) FROM student GROUP BY grade ORDER BY grade;
+
+-- Having Clause
+
+SELECT COUNT(name),city FROM student GROUP BY city HAVING MAX(marks) > 90; 
+
+--  General Order 
+--  SELECT COLUMN(S)
+-- FROM TABLE_NAME
+-- WHERE condition
+-- GROUP BY column(s)
+-- HAVING condition
+-- ORDER BY column(s) 
+
+SELECT DISTINCT city FROM student;
+
+SELECT city FROM student WHERE grade = 'A';
