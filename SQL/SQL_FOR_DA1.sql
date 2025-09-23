fa5c820858ce4345ad9e86a06f54b16ab00211e7
Ankit Bhagat <bhagatankit502@gmail.com>
2025-09-23 22:12:10 +0530

    SQL for DA Ongoing

--- SQL/SQL_FOR_DA1.sql
+++ SQL/SQL_FOR_DA1.sql
@@ -4,4 +4,9 @@ SELECT SaleDate, Amount, Customers FROM sales;
 
 SELECT Amount,Customers,GeoID FROM sales;
 
-SELECT SaleDate, Amount, Boxes, Amount/Boxes FROM sales;
+SELECT SaleDate, Amount, Boxes, Amount/Boxes FROM sales
+
+SELECT SaleDate, COUNT(*) AS duplicate_count
+FROM sales
+GROUP BY SaleDate
+HAVING COUNT(*) > 1;
